0901aec656fe18c6dd144aa151dc86737b3809ef
D <s90789@outlook.com>
2018-02-25 23:54:05 +0800

    add KeyboardPlusPlusManager

--- common/src/main/java/com/d/lib/common/utils/keyboard/KeyboardHelper.java
+++ common/src/main/java/com/d/lib/common/utils/keyboard/KeyboardHelper.java
@@ -20,12 +20,12 @@ import com.d.lib.common.utils.ViewHelper;
  * https://github.com/yshrsmz/KeyboardVisibilityEvent/blob/master/keyboardvisibilityevent/src/main/java/net/yslibrary/android/keyboardvisibilityevent/KeyboardVisibilityEvent.java
  */
 public class KeyboardHelper {
+    private static final String TAG = "QMUIKeyboardHelper";
     /**
      * 显示软键盘的延迟时间
      */
-    public static final int SHOW_KEYBOARD_DELAY_TIME = 200;
-    private static final String TAG = "QMUIKeyboardHelper";
-    private final static int KEYBOARD_VISIBLE_THRESHOLD_DP = 100;
+    public final static int SHOW_KEYBOARD_DELAY_TIME = 200;
+    public final static int KEYBOARD_VISIBLE_THRESHOLD_DP = 100;
 
     /**
      * 针对给定的editText显示软键盘（editText会先获得焦点）. 可以和{@link #hideKeyboard(View)}
